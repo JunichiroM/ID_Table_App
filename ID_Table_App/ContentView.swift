791027a3a384ccebcffc373ab1a65161f86f45da
JunichiroM <sbxn4mxt@yahoo.co.jp>
2025-02-19 16:28:43 +0900

    Initial Commit

---
+++ ID_Table_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ID_Table_App
+//
+//  Created by apple on 2025/02/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ID_Table_App/ID_Table_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ID_Table_AppApp.swift
+//  ID_Table_App
+//
+//  Created by apple on 2025/02/19.
+//
+
+import SwiftUI
+
+@main
+struct ID_Table_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
